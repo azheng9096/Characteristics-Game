074892790314894e6ebf5b84b4550e90c999f379
jbautista5435 <71132262+jbautista5435@users.noreply.github.com>
2023-02-13 13:52:53 -0500

    some player and enemy stuff

--- Assets/MonsterDamage.cs
+++ Assets/MonsterDamage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MonsterDamage : MonoBehaviour
 {
     public int damage;
+    public int health = 100;
     public PlayerHealth playerHealth;
 
     private void OnCollisionEnter2D(Collision2D collision){
@@ -12,5 +13,21 @@ public class MonsterDamage : MonoBehaviour
             playerHealth.TakeDamage(damage);
         }
 
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("Bullet")){
+            health -= damage;
+        }
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
     }
 }
