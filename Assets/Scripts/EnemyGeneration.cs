3ccd04eca61cd4c36a32421890646d7c9a136c0d
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2023-02-13 17:39:27 -0500

    enemy spawner/generator

--- Assets/Scripts/EnemyGeneration.cs
+++ Assets/Scripts/EnemyGeneration.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class EnemyGeneration : MonoBehaviour
 {
+    [SerializeField] Transform[] spawnpointsPool;
+    [SerializeField] int minEnemiesCount, maxEnemiesCount;
+
+    [SerializeField] public List<GameObject> enemyPrefabs;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int numEnemies = Random.Range(minEnemiesCount, maxEnemiesCount);
+        GenerateEnemies(numEnemies);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GenerateEnemies(int num) {
+        List<Transform> spawnpoints = new List<Transform>(spawnpointsPool);
+
+        for (int i = 0; i < num; i++) {
+            if (spawnpoints.Count > 0) {
+                int r = Random.Range(0, enemyPrefabs.Count);
+                int randSpawnpoint = Random.Range(0, spawnpoints.Count);
+
+                GameObject enemy = Instantiate(enemyPrefabs[r], spawnpoints[randSpawnpoint].position, transform.rotation);
+
+                spawnpoints.RemoveAt(randSpawnpoint);
+            }
+        }
     }
 }
