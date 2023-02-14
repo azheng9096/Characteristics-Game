413b8933ce2cd3b8166d946493d30feb9de09f77
jbautista5435 <71132262+jbautista5435@users.noreply.github.com>
2023-02-14 00:23:10 -0500

    some bullet stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,20 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     public float health = 75f;
-    public float maxHealth = 100f;
-    
+    public float maxHealth = 100f;    
+    public float movementSpeed = 7.5f;
+
+
+    public Transform RANGEDPOINT;
+    public GameObject bullet;
+    public float bulletForce;
+
     public float atkPower = 20f;
     public float atkSpeed = 1f;
-    public float movementSpeed = 7.5f;
     
+    
+
+
     Rigidbody2D rb;
 
     public delegate void OnHealthChangedDelegate();
@@ -46,6 +54,18 @@ public class PlayerController : MonoBehaviour
 
         // health
         health = Mathf.Clamp(health, 0, maxHealth);
+
+
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        GameObject bullts = Instantiate(bullet, RANGEDPOINT.position, RANGEDPOINT.rotation);
+        Rigidbody2D rb = bullts.GetComponent<Rigidbody2D>();
+        rb.AddForce(RANGEDPOINT.up * bulletForce, ForceMode2D.Impulse);
     }
 
     public void TakeDamage(float dmg) {
