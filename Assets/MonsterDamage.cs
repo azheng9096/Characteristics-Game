a03058f81b2cfbe97b378d959cc16e915c521c24
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2023-02-14 20:41:19 -0500

    refactor damage code

--- Assets/MonsterDamage.cs
+++ Assets/MonsterDamage.cs
@@ -8,7 +8,6 @@ public class MonsterDamage : MonoBehaviour
     public float health = 100;
     
     PlayerController damages;
-    // public PlayerHealth playerHealth;
 
     PlayerController player;
     ColoredFlash flash;
@@ -20,20 +19,19 @@ public class MonsterDamage : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision){
         if (collision.gameObject.tag == "Player"){
-            // playerHealth.TakeDamage(damage);
             player.TakeDamage(damage);
         }
+    }
 
-        else if (collision.gameObject.CompareTag("Bullet")){
-            float damage = player.returnDamage();
-            flash.Flash(Color.red);
-            health -= damage;
-            if (health <= 0)
-            {
-                Die();
-            }
+    public void TakeDamage(float dmg) {
+        flash.Flash(Color.red);
+        health -= dmg;
+
+        if (health <= 0) {
+            Die();
         }
     }
+
     public void Die() // made public for debugging purposes
     {
         GameOverUI.instance.DeductEnemyCount();
--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    public float lifespanTime = 20f;
     public GameObject hitEffect;
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        GameObject effect = Instantiate (hitEffect, transform.position, Quaternion.identity);
-        Destroy (effect, 5f);
-        Destroy (gameObject);
+    PlayerController player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+        StartCoroutine(Lifespan(lifespanTime));
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Enemy")) {
+            GameObject effect = Instantiate (hitEffect, transform.position, Quaternion.identity);
+            Destroy (effect, 5f);
+
+            MonsterDamage enemy = other.GetComponent<MonsterDamage>();
+            enemy.TakeDamage(player.atkPower);
+
+            Destroy (gameObject);
+        }
+    }
+
+    IEnumerator Lifespan(float time) {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
     }
 }
